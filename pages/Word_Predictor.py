f9ae7924e53eed580edfb10e0af2a1b300c4a3f6
suzal kachhadiya <suzalkachhadiya111@gmail.com>
2024-07-13 15:39:20 +0530

    predictin page modified

--- pages/Word_Predictor.py
+++ pages/Word_Predictor.py
@@ -3,8 +3,38 @@ from NWPproject.pipline import prediction_pipeline
 from NWPproject.config.configuration import ConfigurationManager
 from NWPproject.components.prediction import PredictionPipeline
 
-user_input = st.text_input("Type something:",placeholder="Type Here")
+st.set_page_config(
+    page_title="Word Predictor", 
+    page_icon="⌨️",
+    layout="wide"
+)
+# st.title(":shield: :blue[Phishing Catcher]")
+
+st.markdown("""
+    <style>
+    .title-center {
+        text-align: center;
+        font-size: 3rem;
+        font-weight: bold;
+        color: #3498db;
+    }
+    .rainbow-divider {
+        height: 2px;
+        background-image: linear-gradient(to right, violet, indigo, blue, green, yellow, orange, red);
+        margin-top: 0;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+st.markdown('<h1 class="title-center">⌨️ Word Predictor ⌨️</h1>', unsafe_allow_html=True)
+st.write("\n")
+st.markdown('<div class="rainbow-divider"></div>', unsafe_allow_html=True)
+
+st.write("\n")
+
+user_input = st.text_input(" Type Two / More Words",placeholder="Type Here")
 st.write(user_input)
+st.write("\n")
 
 # print(type(user_input))
 
@@ -15,6 +45,10 @@ if user_input:
     predicted_words=prediction.predict(user_input)
 
     print(predicted_words)
+
+    # st.header("", divider="rainbow")
+
+    st.markdown('<div class="rainbow-divider"></div>', unsafe_allow_html=True)
 
     st.write(predicted_words[0])
     st.write(predicted_words[1])
